fb1b76b753dd4fae64ed567b642a850604f6e651
kkleongsmu <kkleong@smu.edu>
2020-10-30 18:17:43 -0500

    Fixed Val?

--- DSList.h
+++ DSList.h
@@ -65,7 +65,7 @@ public:
     ~DSList(){
         DSNode<Type> *temp = head;
         if(head!= nullptr) {
-            for (int i = 0; i < size; i++) {
+            for (int i = 0; i < size - 1; i++) {
                 delete temp;
                 temp = temp->next;
             }
